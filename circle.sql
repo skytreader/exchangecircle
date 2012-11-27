7dfc7e2d8ba20c642a395270453e6e34fb461817
root <root@chad-MS-7529.(none)>
2012-11-27 17:06:31 +0800

    Merged confirmation codes into invited table in the database. Added more functions in the models. Added connection parameters to functions.

--- circle.sql
+++ circle.sql
@@ -1,3 +1,6 @@
+/**
+This table will have one and only one record.
+*/
 CREATE TABLE event_settings(
 	event_name VARCHAR(50) NOT NULL,
 	confirm_deadline DATE NOT NULL
@@ -7,17 +10,7 @@ CREATE TABLE invited(
 	invitation_id INTEGER AUTO INCREMENT NOT NULL,
 	name VARCHAR(255) NOT NULL,
 	email VARCHAR(50),
+	confirmation_code CHAR(255) UNIQUE NOT NULL,
 	date_confirmed TIMESTAMP,
 	PRIMARY KEY invitation_id
-) ENGINE=INNODB;
-
-/**
-Security table:
-For email for email confirmations.
-*/
-CREATE TABLE mail_confirmations(
-	invitation_id INTEGER NOT NULL,
-	confirmation_code CHAR(255),
-	has_confirmed BOOLEAN DEFAULT FALSE,
-	FOREIGN KEY invitation_id REFERENCES invited
 ) ENGINE=INNODB;
--- xchange_model.php
+++ xchange_model.php
@@ -12,14 +12,51 @@ function generate_confirmation_code(){
 }
 
 /**
-Adds a participant with the given name to the database. The participant
-is added in two tables: invited and mail_confirmations
+Adds a participant with the given name to the database.
 
 Returns true if participant was successfuly added, false otherwise.
+
+TODO Handle 1/(26^255) case when an already-inserted confirmation code is generated
+again.
 */
-function add_participant($name){
-	$insert_query = mysqli_real_escape_string("INSERT INTO invited (name) VALUES ($name)");
-	return mysqli_query($insert_query);
+function add_participant($connection, $name){
+	$confirmation_code = generate_confirmation_code();
+	$insert_invited_query = mysqli_real_escape_string("INSERT INTO invited (name, confirmation_code) VALUES ($name, $confirmation_code)");
+	return mysqli_query($connection, $insert_invited_query);
 }
+
+/**
+Called when a guest confirms attendance to the event.
+
+Send an email to the guest with a confirmation code. Only when
+the guest confirms through this is he considered "attending".
+*/
+function rsvp($connection, $invitation_id, $email){
+	// TODO implement!
+}
+
+/**
+Called when a guest confirms via email. Guests should finish confirmation
+before the set confirmation deadline (in event_settings table).
+
+Returns the result set for the record if the given confirmation code matches
+the invitation id. False, otherwise.
+
+TODO Must send another email to the guest that rsvp is confirmed.
+*/
+function confirm($connection, $invitation_id, $confirmation_code){
+	// Get the confirmation deadline and check server date if it is not yet over.
+	$confirmation_date_query = mysqli_real_escape_string("SELECT confirmation_deadline FROM event_settings LIMIT 1;");
+	$query_result = mysqli_query($confirmation_date_query);
+
+	// We can safely assume that the invitation id and confirmation code is
+	// already in the DB iff it is a valid combination.
+	$check_query = mysqli_real_escape_string("SELECT * FROM invited WHERE invitation_id = '$invitation_id' AND confirmation_code = '$confirmation_code' LIMIT 1;");
+	return mysqli_query($connection, $check_query);
+}
+
+/**
+
+*/
 
 ?>
