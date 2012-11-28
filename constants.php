b07d1ae14bdf1633d5fd8e6754104d9c0c2267a1
root <root@chad-MS-7529>
2012-11-28 19:06:22 +0800

    Start work on front-end index.php .

---
+++ constants.php
@@ -0,0 +1,7 @@
+<?php
+	// TODO _#ALWAYS#_ COMMIT TEST CONNECTION CREDENTIALS _#ONLY#_
+	define(DB_HOST, "localhost");
+	define(DB_USERNAME, "root");
+	define(DB_PASSWORD, "");
+	define(DB_NAME, "xchange");
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	// ALWAYS COMMIT 
+	$db_connection = mysqli_connect
+?>
+<html>
+	<head>
+		<title>Exchange Circle</title>
+	</head>
+	<body>
+		<!-- Check if there is an event configured in the database and adapt display accordingly. -->
+
+	</body>
+</html>
